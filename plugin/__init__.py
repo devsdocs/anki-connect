068b7ecf5cad0c11fd33ef92fffa9771ac8eed5b
R-J Lim <kgerbil@gmail.com>
2025-11-06 09:13:37 +0900

    Fix 'fields' key check when adding media

--- plugin/__init__.py
+++ plugin/__init__.py
@@ -785,7 +785,7 @@ class AnkiConnect:
                                                         skipHash=media.get('skipHash'),
                                                         deleteExisting=media.get('deleteExisting'))
 
-                    if mediaFilename is not None and hasattr(media, 'fields'):
+                    if mediaFilename is not None and 'fields' in media and type(media['fields']) == list:
                         for field in media['fields']:
                             if field in ankiNote:
                                 if mediaType is util.MediaType.Picture:
